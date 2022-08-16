0a186ace94ec0cfcacc4a367344d9dec08e70e46
Andrei Shelkovyi <sigertraciy@yandex.ru>
2022-08-17 02:15:46 +0300

    Added the sales quantity method

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int soldProduct[] = {10, 20, 30, 40, 50};
+        int soldProduct[] = {10, 2, 30, 100, 50, 23, 45};
 
         SalesManager salesManager = new SalesManager(soldProduct);
         salesManager.max();
 
         System.out.print("Статистика продаж: ");
-        System.out.println(salesManager.max());
+        System.out.println("Максимальная продажа: " + salesManager.max());
+        System.out.println("Минимальная продажа: " + salesManager.min());
+
+        System.out.println("Среднее количество продаж: " + salesManager.avgTrimmed());
 
     }
 
